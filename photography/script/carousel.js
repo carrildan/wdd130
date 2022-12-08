5fff45b38c66a0061b256787866036ddfdbfe2d1
carrildan <97158875+carrildan@users.noreply.github.com>
2022-12-08 02:55:10 +0100

    updated gallery and contactus. Created carousel.js

---
+++ photography/script/carousel.js
@@ -0,0 +1,88 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// ------------------- Slide 2 -----------------
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n2) {
+  showSlides2(slideIndex2 += n2);
+}
+
+// Thumbnail image controls
+function currentSlide2(n2) {
+  showSlides2(slideIndex2 = n2);
+}
+
+function showSlides2(n2) {
+  let i;
+  let slides2 = document.getElementsByClassName("mySlides2");
+  let dots2 = document.getElementsByClassName("dot2");
+  if (n2 > slides2.length) {slideIndex2 = 1}
+  if (n2 < 1) {slideIndex2 = slides2.length}
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";
+  }
+  for (i = 0; i < dots2.length; i++) {
+    dots2[i].className = dots2[i].className.replace(" active", "");
+  }
+  slides2[slideIndex2-1].style.display = "block";
+  dots2[slideIndex2-1].className += " active";
+}
+
+// ------------------- SLIDE 3 ------------------
+let slideIndex3 = 1;
+showSlides3(slideIndex3);
+
+// Next/previous controls
+function plusSlides3(n3) {
+  showSlides3(slideIndex3 += n3);
+}
+
+// Thumbnail image controls
+function currentSlide3(n3) {
+  showSlides3(slideIndex3 = n3);
+}
+
+function showSlides3(n3) {
+  let i;
+  let slides3 = document.getElementsByClassName("mySlides3");
+  let dots3 = document.getElementsByClassName("dot3");
+  if (n3 > slides3.length) {slideIndex3 = 1}
+  if (n3 < 1) {slideIndex3 = slides3.length}
+  for (i = 0; i < slides3.length; i++) {
+    slides3[i].style.display = "none";
+  }
+  for (i = 0; i < dots3.length; i++) {
+    dots3[i].className = dots3[i].className.replace(" active", "");
+  }
+  slides3[slideIndex3-1].style.display = "block";
+  dots3[slideIndex3-1].className += " active";
+}
